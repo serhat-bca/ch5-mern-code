0e920d34e8f1e53bcfdfa574b52d09a490a2ad1e
serhat-bca <sersen@bergen.org>
2024-12-10 16:08:09 -0500

    model folder created.

--- movies-fullstack/movies-backend/index.js
+++ movies-fullstack/movies-backend/index.js
@@ -1,41 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// Movie model
+const Movie = require("./models/movie");
+
 // middleware for parsing body into js object
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
-// require dotenv
-require("dotenv").config();
-// require mongoose
-const mongoose = require("mongoose");
-
-const DB_URI = process.env.MONGODB_URI;
-// mongoose setup and connection
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(DB_URI)
-  .then(() => console.log("DB Connection Established"))
-  .catch((e) => {
-    console.log("Error connecting the DB: ", e.message);
-  });
-
-// create movie schema
-const movieSchema = new mongoose.Schema({
-  title: String,
-  watchlist: Boolean,
-});
-
-// configure toJSON method
-movieSchema.set("toJSON", {
-  transform: (doc, ret) => {
-    ret.id = ret._id.toString();
-    delete ret._id;
-  },
-});
-
-// create mongoose model
-const Movie = mongoose.model("Movie", movieSchema);
 
 const requestLogger = (req, res, next) => {
   console.log(`Request Method: ${req.method}`);
---
+++ movies-fullstack/movies-backend/models/movie.js
@@ -0,0 +1,33 @@
+// require dotenv
+require("dotenv").config();
+// require mongoose
+const mongoose = require("mongoose");
+
+const DB_URI = process.env.MONGODB_URI;
+// mongoose setup and connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log("DB Connection Established"))
+  .catch((e) => {
+    console.log("Error connecting the DB: ", e.message);
+  });
+
+// create movie schema
+const movieSchema = new mongoose.Schema({
+  title: String,
+  watchlist: Boolean,
+});
+
+// configure toJSON method
+movieSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+  },
+});
+
+// create mongoose model
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
